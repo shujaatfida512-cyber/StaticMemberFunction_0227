80c258e50eba01cd0824af2efe778b70e72f7e4b
Hussain512 <shujaatfida512@gmail.com>
2026-05-12 11:54:52 +0700

    `Create angka class with dynamic array`

--- destructor.cpp
+++ destructor.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class angka
+{
+private:
+    int* arr;
+    int panjang;
+
+public:
+    angka(int);
+    ~angka();
+    void cetakData();
+    void isiData();
+};
 
 int main()
 {
