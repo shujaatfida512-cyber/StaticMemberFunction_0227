132390a69f2829102d68c38ebc568dfdee42a295
Hussain512 <shujaatfida512@gmail.com>
2026-05-12 11:53:17 +0700

    Start of destructor program

--- destructor.cpp
+++ destructor.cpp
@@ -1,21 +1,8 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa
-{
-public:
-	mahasiswa();
-};
-
-mahasiswa::mahasiswa()
-{
-	cout << "Constructor: " << endl;
-};
-
-
 
 int main()
 {
-	mahasiswa mhs;
-	return 0;
+
 }
