ad922b0b4c2c631419cea70a627b4be28da5cc23
Hussain512 <shujaatfida512@gmail.com>
2026-05-12 12:01:05 +0700

    Add parameterized mahasiswa constructor`

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa {
+public:
+    mahasiswa(int nim, string nama);
+};
+
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor Dengan Parameter Terpanggil" << endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
+
+int main () 
+{
+
+}
