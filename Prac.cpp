91608b0c3f2b472e463007a8250d14732add0243
Hussain512 <shujaatfida512@gmail.com>
2026-05-12 17:09:18 +0700

    Add Vehicle class with multiple constructors

--- Prac.cpp
+++ Prac.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 using namespace std;
 
+class Vehicle
+{
+private:
+    int vehicleID;
+    string vehicleName;
+
+public:
+    Vehicle()
+    {
+        vehicleID = 0;
+        vehicleName = "Unknown";
+    }
+
+    Vehicle(int id)
+    {
+        vehicleID = id;
+        vehicleName = "Unknown";
+    }
+
+    Vehicle(string name)
+    {
+        vehicleID = 0;
+        vehicleName = name;
+    }
+
+    Vehicle(int id, string name)
+    {
+        vehicleID = id;
+        vehicleName = name;
+    }
+
 int main()
 {
 
