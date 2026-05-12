077a13eb842a8fd257b3006b9246099f74da0106
Hussain512 <shujaatfida512@gmail.com>
2026-05-12 10:07:50 +0700

    using class mahasiswa

--- P2.cpp
+++ P2.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 using namespace std;
- 
+
+class Mahasiswa
+{
+public:
+	static int nim;
+	int id;
+	string name;
+
+	void setID();
+	void printAll();
+
+	Mahasiswa(string pnama) :name(pnama)
+	{
+		setID();
+	}
+};
+
 int main() 
 {
-
+	
 }
