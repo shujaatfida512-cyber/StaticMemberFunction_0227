448ff7c1b82dd422b6c7ee01904fc4ecec2641cf
Hussain512 <shujaatfida512@gmail.com>
2026-05-12 09:57:43 +0700

    Declaring class having public

--- StaticMemberFunction_0227.cpp
+++ StaticMemberFunction_0227.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 using namespace std;
+
+class buku
+{
+    string judul;
+public:
+    void setJudul(string judul)
+    {
+        this->judul = judul;
+    }
+
+    string getJudul()
+    {
+        return this->judul;
+    }
+};
+
 int main()
 {
 
