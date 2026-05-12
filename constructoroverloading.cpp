903a3bc108bc9b785ebb17f5f76eff3f9507e936
Hussain512 <shujaatfida512@gmail.com>
2026-05-12 12:04:37 +0700

    Implement overloaded mahasiswa constructors`

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -14,6 +14,22 @@ public:
     void cetak();
 };
 
+mahasiswa::mahasiswa() {
+}
+
+mahasiswa::mahasiswa(int iNim) {
+    nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama) {
+    nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+    nim = iNim;
+    nama = iNama;
+}
+
 int main()
 {
 
