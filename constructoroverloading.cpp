a1de38cec72e88ab1a8847f65207a7e0a35a6c74
Hussain512 <shujaatfida512@gmail.com>
2026-05-12 12:05:13 +0700

    Add mahasiswa print method

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -30,6 +30,11 @@ mahasiswa::mahasiswa(int iNim, string iNama) {
     nama = iNama;
 }
 
+void mahasiswa::cetak() {
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama= " << nama << endl;
+}
+
 int main()
 {
 
