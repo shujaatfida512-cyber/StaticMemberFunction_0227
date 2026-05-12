392bd468366104c7a6170311fabb387ad74d4647
Hussain512 <shujaatfida512@gmail.com>
2026-05-12 12:03:55 +0700

    Add overloaded constructors to mahasiswa class`

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -2,7 +2,17 @@
 #include <string>
 using namespace std;
 
-
+class mahasiswa {
+private:
+    int nim;
+    string nama;
+public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
 
 int main()
 {
