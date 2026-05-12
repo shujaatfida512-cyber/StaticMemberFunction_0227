ce53eb5c5914e417e488aa121911558103d5983c
Hussain512 <shujaatfida512@gmail.com>
2026-05-12 10:23:31 +0700

    write class student

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa
+{
 
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	static void setNim(int pnim)
+	{
+		nim = pnim;
+	};
+
+	static int getNim()
+	{
+		return nim;
+	}
+
+	Mahasiswa(string pnama) : nama(pnama) {
+		setID();
+	};
+};
 
 int main()
 {
