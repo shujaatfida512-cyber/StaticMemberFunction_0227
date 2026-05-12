34e003f42a84d99ff2b22a4de78edcee96f8155e
Hussain512 <shujaatfida512@gmail.com>
2026-05-12 11:58:36 +0700

    `Add buku class with setter and getter`

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class buku {
+    string judul;
+
+public:
+    buku setJudul(string judul) {
+        this->judul = judul;
+        return *this;
+    }
+    string getJudul() {
+        return this->judul;
+    }
+};
+
 int main()
 {
 
